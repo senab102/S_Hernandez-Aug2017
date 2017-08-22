15d8f0ce38f9032facc14608fb48446a35d2c6ad
Sena Hernandez <18Hernandezs20@millshighsch.org>
2017-08-22 09:28:41 -0700

    Second Project -- Using Git from eclipse

---
+++ Project2/src/Project2.java
@@ -0,0 +1,14 @@
+//Sena Hernandez
+//My second java project. It will compare print and printin.
+//August 22,2017
+
+public class Project2 {
+
+	public static void main(String[] args) {
+	System.out.println("hello -- this is project 2!!");
+
+	System.out.print("hello -- this is project 2!!");
+	
+	}
+	
+}
