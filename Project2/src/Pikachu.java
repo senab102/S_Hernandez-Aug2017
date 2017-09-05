3b0ec1dc1fabbbc3d3d13e35c171c2208598ac87
Sena Hernandez <18Hernandezs20@millshighsch.org>
2017-09-05 09:28:52 -0700

    Calculate Library

---
+++ Project2/src/Pikachu.java
@@ -0,0 +1,12 @@
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\t Pikachu welcomes you to the world of Polemon!");
+		System.out.println("\t\t\t   (\\__/)");
+		System.out.println("\t\t\t   (^.^)\"");
+		System.out.println("\t\t\tz(_(\\\")(\"\\)");		
+	}
+
+}
