91091b1f833ca5393093955a565ed579025c6c45
Sena Hernandez <18Hernandezs20@millshighsch.org>
2017-09-05 09:31:24 -0700

    tues calculate

---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,21 @@
+//Sena Hernandez
+//September 5,2017
+//This is our home grown library that will contain various mathematical methods.
+public class Calculate {
+
+	//This method takes an integer and returns its square.
+	public static int square (int number) {
+		return number * number;
+	}
+	//This method takes an integer and returns its cube.
+	public static int cube (int number) {
+		return number * number * number;
+	}
+	//This method takes a double and returns an average of the two numbers.
+	public static double average (double number1, double number2) {
+		return (number1 + number2 )/2;
+	}
+	//This method
+	public static double average (double number1, double number2, double number3) {
+
+}
