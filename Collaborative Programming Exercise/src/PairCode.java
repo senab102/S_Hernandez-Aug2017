75da3c9b51ad5b110ca8442b1b7f6dded3952af7
Sena Hernandez <18Hernandezs20@millshighsch.org>
2017-10-03 09:25:20 -0700

    class

---
+++ Collaborative Programming Exercise/src/PairCode.java
@@ -0,0 +1,23 @@
+
+import java.util.* ;
+public class PairCode {
+	Scanner input = new Scanner (System.in);
+	boolean done = false;
+	int SumEven = 0;
+	int MinNum = 0;
+	int MaxNum = 0;
+	int n = input.nextInt();
+for (i=0; i<n; i++) {
+	num = input.nextInt ();
+	if (num>large) {
+		large = num;
+	System.out.println("Are you done?");
+	String finished = Scanner.next();
+	if (finished.equals("yes")) {
+		done = true;
+	}
+}
+		
+
+}
+}
